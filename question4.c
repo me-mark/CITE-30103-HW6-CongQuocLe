697d07b98aa5c42d0ff3eda88eaeccfc9a2b92cb
cong.le <cong.le@tcu.edu>
2025-11-12 22:13:16 -0600

    Finished question4

---
+++ question4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 5, b = 10;
+    float avg = (a + b) / 2.0;    // use floating-point division
+    printf("Average = %.2f\n", avg);
+    return 0;
+}
