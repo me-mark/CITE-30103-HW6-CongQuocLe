dd5c01041e96281b01c835d4bd6bb281920654c2
cong.le <cong.le@tcu.edu>
2025-11-14 09:55:04 -0600

    Finished question21

---
+++ question21.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int rows;
+    int i, j, space;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; i++)
+    {
+        /* Print leading spaces */
+        for (space = 1; space <= rows - i; space++)
+        {
+            printf(" ");
+        }
+
+        /* Print stars */
+        for (j = 1; j <= (2*i - 1); j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
